7baa969a307dff49390e3c50783074edc9fc9d3f
Cauesilvaa <43147371+Cauesilvaa@users.noreply.github.com>
2023-01-12 17:05:37 -0300

    start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className='div-fundo-app'>
+        <h1>teste</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
