75435e5f771ab4d2b366c61473b0f2131d985735
Cauesilvaa <43147371+Cauesilvaa@users.noreply.github.com>
2023-04-07 20:58:34 +0044

    estrurura do formulario de compra finalizado

--- src/components/formValidationBuy/FormValidationBuy.jsx
+++ src/components/formValidationBuy/FormValidationBuy.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Alert from 'react-bootstrap/Alert';
 import {
   Progress,
   Box,
@@ -170,168 +171,52 @@ const Form2 = () => {
       </Flex>
 
       <FormControl as={GridItem} colSpan={6}>
-        <FormLabel
-          htmlFor="street_address"
-          fontSize="sm"
-          fontWeight="md"
-          color="gray.700"
-          _dark={{
-            color: 'gray.50',
-          }}
-          mt="2%">
-          Street address
+
+        <FormLabel htmlFor="street_address" fontSize="sm" fontWeight="md" color="gray.700" _dark={{ color: 'gray.50', }} mt="2%">
+          Pix copia e cola
         </FormLabel>
-        <Input
-          type="text"
-          name="street_address"
-          id="street_address"
-          autoComplete="street-address"
-          focusBorderColor="brand.400"
-          shadow="sm"
-          size="sm"
-          w="full"
-          rounded="md"
-        />
+
+        <Input type="text" name="street_address" id="street_address" autoComplete="street-address" focusBorderColor="brand.400"
+          shadow="sm" size="sm" w="full" rounded="md" />
       </FormControl>
 
       <FormControl as={GridItem} colSpan={[6, 6, null, 2]}>
-        <FormLabel
-          htmlFor="city"
-          fontSize="sm"
-          fontWeight="md"
-          color="gray.700"
-          _dark={{
-            color: 'gray.50',
-          }}
-          mt="2%">
-          City
+
+        <FormLabel htmlFor="city" fontSize="sm" fontWeight="md" color="gray.700" _dark={{ color: 'gray.50', }} mt="2%">
+          Código do boleto
         </FormLabel>
-        <Input
-          type="text"
-          name="city"
-          id="city"
-          autoComplete="city"
-          focusBorderColor="brand.400"
-          shadow="sm"
-          size="sm"
-          w="full"
-          rounded="md"
-        />
+
+        <Input type="text" name="city" id="city" autoComplete="city" focusBorderColor="brand.400" shadow="sm" size="sm" w="full" rounded="md"/>
       </FormControl>
 
-      <FormControl as={GridItem} colSpan={[6, 3, null, 2]}>
-        <FormLabel
-          htmlFor="state"
-          fontSize="sm"
-          fontWeight="md"
-          color="gray.700"
-          _dark={{
-            color: 'gray.50',
-          }}
-          mt="2%">
-          State / Province
-        </FormLabel>
-        <Input
-          type="text"
-          name="state"
-          id="state"
-          autoComplete="state"
-          focusBorderColor="brand.400"
-          shadow="sm"
-          size="sm"
-          w="full"
-          rounded="md"
-        />
-      </FormControl>
-
-      <FormControl as={GridItem} colSpan={[6, 3, null, 2]}>
-        <FormLabel
-          htmlFor="postal_code"
-          fontSize="sm"
-          fontWeight="md"
-          color="gray.700"
-          _dark={{
-            color: 'gray.50',
-          }}
-          mt="2%">
-          ZIP / Postal
-        </FormLabel>
-        <Input
-          type="text"
-          name="postal_code"
-          id="postal_code"
-          autoComplete="postal-code"
-          focusBorderColor="brand.400"
-          shadow="sm"
-          size="sm"
-          w="full"
-          rounded="md"
-        />
-      </FormControl>
     </>
   );
 };
 
 const Form3 = () => {
   return (
-    <>
-      <Heading w="100%" textAlign={'center'} fontWeight="normal">
-        Social Handles
-      </Heading>
-      <SimpleGrid columns={1} spacing={6}>
-        <FormControl as={GridItem} colSpan={[3, 2]}>
-          <FormLabel
-            fontSize="sm"
-            fontWeight="md"
-            color="gray.700"
-            _dark={{
-              color: 'gray.50',
-            }}>
-            Website
-          </FormLabel>
-          <InputGroup size="sm">
-            <InputLeftAddon
-              bg="gray.50"
-              _dark={{
-                bg: 'gray.800',
-              }}
-              color="gray.500"
-              rounded="md">
-              http://
-            </InputLeftAddon>
-            <Input
-              type="tel"
-              placeholder="www.example.com"
-              focusBorderColor="brand.400"
-              rounded="md"
-            />
-          </InputGroup>
-        </FormControl>
+    <>      
+      <Alert variant="primary">
+      <Alert.Heading>Dados da compra</Alert.Heading>
+      <hr />
+      <p className="mb-0">
+        Aqui vai ficar os dados
+      </p>
+    </Alert>
+    </>
+  );
+};
 
-        <FormControl id="email" mt={1}>
-          <FormLabel
-            fontSize="sm"
-            fontWeight="md"
-            color="gray.700"
-            _dark={{
-              color: 'gray.50',
-            }}>
-            About
-          </FormLabel>
-          <Textarea
-            placeholder="you@example.com"
-            rows={3}
-            shadow="sm"
-            focusBorderColor="brand.400"
-            fontSize={{
-              sm: 'sm',
-            }}
-          />
-          <FormHelperText>
-            Brief description for your profile. URLs are hyperlinked.
-          </FormHelperText>
-        </FormControl>
-      </SimpleGrid>
+const MessageSucces = () => {
+  return (
+    <>      
+      <Alert variant="primary">
+      <Alert.Heading>Compra finalizada com sucesso</Alert.Heading>
+      <hr />
+      <p className="mb-0">
+        Confira o status da compra em seu email
+      </p>
+    </Alert>
     </>
   );
 };
@@ -342,57 +227,36 @@ export default function multistep() {
   const [progress, setProgress] = useState(33.33);
   return (
     <>
-      <Box
-        borderWidth="1px"
-        rounded="lg"
-        shadow="1px 1px 3px rgba(0,0,0,0.3)"
-        maxWidth={800}
-        p={6}
-        m="10px auto"
-        as="form">
-        <Progress
-          hasStripe
-          value={progress}
-          mb="5%"
-          mx="5%"
-          isAnimated></Progress>
+      <Box borderWidth="1px" rounded="lg" shadow="1px 1px 3px rgba(0,0,0,0.3)" maxWidth={800} p={6} m="10px auto" as="form">
+
+        <Progress hasStripe value={progress} mb="5%" mx="5%" isAnimated></Progress>
+        
         {step === 1 ? <Form1 /> : step === 2 ? <Form2 /> : <Form3 />}
+
         <ButtonGroup mt="5%" w="100%">
           <Flex w="100%" justifyContent="space-between">
             <Flex>
-              <Button
-                onClick={() => {
-                  setStep(step - 1);
-                  setProgress(progress - 33.33);
-                }}
-                isDisabled={step === 1}
-                colorScheme="teal"
-                variant="solid"
-                w="7rem"
-                mr="5%">
+              
+              <Button onClick={() => { setStep(step - 1); setProgress(progress - 33.33); }} isDisabled={step === 1} colorScheme="teal"
+                variant="solid" w="7rem" mr="5%">
                 Back
               </Button>
-              <Button
-                w="7rem"
-                isDisabled={step === 3}
-                onClick={() => {
-                  setStep(step + 1);
+
+              <Button w="7rem" isDisabled={step === 3} colorScheme="teal" variant="outline"
+                onClick={() => { setStep(step + 1);
                   if (step === 3) {
                     setProgress(100);
                   } else {
                     setProgress(progress + 33.33);
                   }
-                }}
-                colorScheme="teal"
-                variant="outline">
+                }} >
                 Next
               </Button>
+
             </Flex>
+
             {step === 3 ? (
-              <Button
-                w="7rem"
-                colorScheme="red"
-                variant="solid"
+              <Button w="15rem" colorScheme="red" variant="solid"
                 onClick={() => {
                   toast({
                     title: 'Account created.',
@@ -402,10 +266,11 @@ export default function multistep() {
                     isClosable: true,
                   });
                 }}>
-                Submit
+                Confirmar compra
               </Button>
             ) : null}
           </Flex>
+
         </ButtonGroup>
       </Box>
     </>
