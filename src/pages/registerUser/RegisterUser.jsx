81952ae8f54e52bd1346b405af5599863a9241be
Cauesilvaa <43147371+Cauesilvaa@users.noreply.github.com>
2023-06-29 17:51:12 -0300

    fazer a validação do formulario com o yup

--- src/pages/registerUser/RegisterUser.jsx
+++ src/pages/registerUser/RegisterUser.jsx
@@ -20,292 +20,211 @@ import {
 } from '@chakra-ui/react';
 
 import { useToast } from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers';
+import { object, string } from 'yup';
 
-const Form1 = () => {
-  const [show, setShow] = React.useState(false);
-  const handleClick = () => setShow(!show);
-  return (
-    <>
-      <Heading w="100%" textAlign={'center'} fontWeight="normal" mb="2%">
-        Realize seu cadastro
-      </Heading>
-      <Flex>
-        <FormControl mr="5%">
-          <FormLabel htmlFor="first-name" fontWeight={'normal'}>
-            First name
+const schema = object({
+  name: string().required()
+})
+
+export default function RegisterUser (){
+
+  const { register, handleSubmit, formState: { errors } } = useForm({resolver: yupResolver(schema)})
+
+  function createUser(data) {
+    console.log(data);
+  }
+
+  const Form1 = () => {
+    const [show, setShow] = React.useState(false);
+    const handleClick = () => setShow(!show);
+    return (
+      <>
+        <Heading w="100%" textAlign={'center'} fontWeight="normal" mb="2%">
+          Realize seu cadastro
+        </Heading>
+
+        <Flex>
+
+          <FormControl mr="5%">
+            <FormLabel htmlFor="first-name" fontWeight={'normal'}>
+              Nome
+            </FormLabel>
+            <Input id="first-name" placeholder="Nome" {...register('name')}/>
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="last-name" fontWeight={'normal'}>
+              Sobrenome
+            </FormLabel>
+            <Input id="last-name" placeholder="Sobrenome" {...register('last_name')}/>
+          </FormControl>
+
+        </Flex>
+
+        <Flex marginTop={'30'}>
+
+          <FormControl mr="5%">
+            <FormLabel htmlFor="birth" fontWeight={'normal'}>
+              Data de Nascimento
+            </FormLabel>
+            <Input id="birth" type='date' {...register('birth')} />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="cpf" fontWeight={'normal'}>
+              CPF
+            </FormLabel>
+            <Input id="cpf" placeholder="000.000.000-00" {...register('cpf')}/>
+          </FormControl>
+
+        </Flex>
+
+        <Flex marginTop={'30'}>
+
+          <FormControl mr="5%">
+            <FormLabel htmlFor="email" fontWeight={'normal'}>
+              Email
+            </FormLabel>
+            <Input id="email" placeholder="email@email.com" {...register('email')}/>
+          </FormControl>
+
+          <FormControl>
+          <FormLabel htmlFor="password" fontWeight={'normal'} mt="2%">
+            Senha
           </FormLabel>
-          <Input id="first-name" placeholder="First name" />
-        </FormControl>
-
-        <FormControl>
-          <FormLabel htmlFor="last-name" fontWeight={'normal'}>
-            Last name
-          </FormLabel>
-          <Input id="last-name" placeholder="First name" />
-        </FormControl>
-      </Flex>
-      <FormControl mt="2%">
-        <FormLabel htmlFor="email" fontWeight={'normal'}>
-          Email address
-        </FormLabel>
-        <Input id="email" type="email" />
-        <FormHelperText>We'll never share your email.</FormHelperText>
-      </FormControl>
-
-      <FormControl>
-        <FormLabel htmlFor="password" fontWeight={'normal'} mt="2%">
-          Password
-        </FormLabel>
-        <InputGroup size="md">
-          <Input
-            pr="4.5rem"
-            type={show ? 'text' : 'password'}
-            placeholder="Enter password"
-          />
-          <InputRightElement width="4.5rem">
-            <Button h="1.75rem" size="sm" onClick={handleClick}>
-              {show ? 'Hide' : 'Show'}
-            </Button>
-          </InputRightElement>
-        </InputGroup>
-      </FormControl>
-    </>
-  );
-};
-
-const Form2 = () => {
-  return (
-    <>
-      <Heading w="100%" textAlign={'center'} fontWeight="normal" mb="2%">
-        User Details
-      </Heading>
-      <FormControl as={GridItem} colSpan={[6, 3]}>
-        <FormLabel
-          htmlFor="country"
-          fontSize="sm"
-          fontWeight="md"
-          color="gray.700"
-          _dark={{
-            color: 'gray.50',
-          }}>
-          Country / Region
-        </FormLabel>
-        <Select
-          id="country"
-          name="country"
-          autoComplete="country"
-          placeholder="Select option"
-          focusBorderColor="brand.400"
-          shadow="sm"
-          size="sm"
-          w="full"
-          rounded="md">
-          <option>United States</option>
-          <option>Canada</option>
-          <option>Mexico</option>
-        </Select>
-      </FormControl>
-
-      <FormControl as={GridItem} colSpan={6}>
-        <FormLabel
-          htmlFor="street_address"
-          fontSize="sm"
-          fontWeight="md"
-          color="gray.700"
-          _dark={{
-            color: 'gray.50',
-          }}
-          mt="2%">
-          Street address
-        </FormLabel>
-        <Input
-          type="text"
-          name="street_address"
-          id="street_address"
-          autoComplete="street-address"
-          focusBorderColor="brand.400"
-          shadow="sm"
-          size="sm"
-          w="full"
-          rounded="md"
-        />
-      </FormControl>
-
-      <FormControl as={GridItem} colSpan={[6, 6, null, 2]}>
-        <FormLabel
-          htmlFor="city"
-          fontSize="sm"
-          fontWeight="md"
-          color="gray.700"
-          _dark={{
-            color: 'gray.50',
-          }}
-          mt="2%">
-          City
-        </FormLabel>
-        <Input
-          type="text"
-          name="city"
-          id="city"
-          autoComplete="city"
-          focusBorderColor="brand.400"
-          shadow="sm"
-          size="sm"
-          w="full"
-          rounded="md"
-        />
-      </FormControl>
-
-      <FormControl as={GridItem} colSpan={[6, 3, null, 2]}>
-        <FormLabel
-          htmlFor="state"
-          fontSize="sm"
-          fontWeight="md"
-          color="gray.700"
-          _dark={{
-            color: 'gray.50',
-          }}
-          mt="2%">
-          State / Province
-        </FormLabel>
-        <Input
-          type="text"
-          name="state"
-          id="state"
-          autoComplete="state"
-          focusBorderColor="brand.400"
-          shadow="sm"
-          size="sm"
-          w="full"
-          rounded="md"
-        />
-      </FormControl>
-
-      <FormControl as={GridItem} colSpan={[6, 3, null, 2]}>
-        <FormLabel
-          htmlFor="postal_code"
-          fontSize="sm"
-          fontWeight="md"
-          color="gray.700"
-          _dark={{
-            color: 'gray.50',
-          }}
-          mt="2%">
-          ZIP / Postal
-        </FormLabel>
-        <Input
-          type="text"
-          name="postal_code"
-          id="postal_code"
-          autoComplete="postal-code"
-          focusBorderColor="brand.400"
-          shadow="sm"
-          size="sm"
-          w="full"
-          rounded="md"
-        />
-      </FormControl>
-    </>
-  );
-};
-
-const Form3 = () => {
-  return (
-    <>
-      <Heading w="100%" textAlign={'center'} fontWeight="normal">
-        Social Handles
-      </Heading>
-      <SimpleGrid columns={1} spacing={6}>
-        <FormControl as={GridItem} colSpan={[3, 2]}>
-          <FormLabel
-            fontSize="sm"
-            fontWeight="md"
-            color="gray.700"
-            _dark={{
-              color: 'gray.50',
-            }}>
-            Website
-          </FormLabel>
-          <InputGroup size="sm">
-            <InputLeftAddon
-              bg="gray.50"
-              _dark={{
-                bg: 'gray.800',
-              }}
-              color="gray.500"
-              rounded="md">
-              http://
-            </InputLeftAddon>
-            <Input
-              type="tel"
-              placeholder="www.example.com"
-              focusBorderColor="brand.400"
-              rounded="md"
-            />
+          
+          <InputGroup size="md">
+            <Input pr="4.5rem" type={show ? 'text' : 'password'} placeholder="Senha" {...register('password')}/>
+            <InputRightElement width="4.5rem">
+              <Button h="1.75rem" size="sm" onClick={handleClick}>
+                {show ? 'Esconder' : 'Exibir'}
+              </Button>
+            </InputRightElement>
           </InputGroup>
         </FormControl>
 
-        <FormControl id="email" mt={1}>
+        </Flex>
+      </>
+    );
+  };
+
+  const Form2 = () => {
+    return (
+      <>
+        <FormControl as={GridItem} colSpan={[6, 3]} marginTop={'5'} mr="5%">
           <FormLabel
+            htmlFor="country"
+            fontWeight={'normal'} mt="2%">
+            Nacionalidade
+          </FormLabel>
+          <Select
+            id="country"
+            name="country"
+            autoComplete="country"
+            placeholder="Selecione"
+            focusBorderColor="brand.400"
+            shadow="sm"
+            size="sm"
+            w="full"
+            rounded="md" {...register('country')}>
+            <option>United States</option>
+            <option>Canada</option>
+            <option>Mexico</option>
+          </Select>
+        </FormControl>
+
+        <FormControl as={GridItem} colSpan={[6, 6, null, 2]}>
+          <FormLabel
+            htmlFor="cep"
             fontSize="sm"
             fontWeight="md"
             color="gray.700"
             _dark={{
               color: 'gray.50',
-            }}>
-            About
-          </FormLabel>
-          <Textarea
-            placeholder="you@example.com"
-            rows={3}
-            shadow="sm"
-            focusBorderColor="brand.400"
-            fontSize={{
-              sm: 'sm',
             }}
+            mt="2%">
+            CEP
+          </FormLabel>
+          <Input
+            type="text"
+            name="cep"
+            id="cep"
+            autoComplete="cep"
+            focusBorderColor="brand.400"
+            shadow="sm"
+            size="sm"
+            w="full"
+            rounded="md"
+            {...register('cep')}
           />
-          <FormHelperText>
-            Brief description for your profile. URLs are hyperlinked.
-          </FormHelperText>
         </FormControl>
-      </SimpleGrid>
-    </>
-  );
-};
 
-export default function multistep() {
-  const toast = useToast();
-  const [step, setStep] = useState(1);
-  const [progress, setProgress] = useState(33.33);
+        <Flex>
+
+          <FormControl mr="5%" marginTop={'20px'}>
+            <FormLabel htmlFor="street" fontWeight={'normal'}>
+              Endereço
+            </FormLabel>
+            <Input id="street" type='text' {...register('street')}/>
+          </FormControl>
+
+          <FormControl marginTop={'20px'}>
+            <FormLabel htmlFor="complement" fontWeight={'normal'}>
+              Complemento
+            </FormLabel>
+            <Input id="complement" type='text' {...register('complement')}/>
+          </FormControl>
+
+        </Flex>
+
+        <Flex>
+
+          <FormControl mr="5%" marginTop={'20px'}>
+            <FormLabel htmlFor="city" fontWeight={'normal'}>
+              Cidade
+            </FormLabel>
+            <Input id="city" type='text' {...register('city')}/>
+          </FormControl>
+
+          <FormControl marginTop={'20px'}>
+            <FormLabel htmlFor="state" fontWeight={'normal'}>
+              Estado
+            </FormLabel>
+            <Input id="state" type='text' {...register('state')}/>
+          </FormControl>
+
+        </Flex>
+      </>
+    );
+  };
+
+  function Multistep() {
+    const toast = useToast();
+    const [step, setStep] = useState(1);
+    const [progress, setProgress] = useState(33.33);
+    return (
+      <>
+        <Box borderWidth="1px" rounded="lg" shadow="1px 1px 3px rgba(0,0,0,0.3)" maxWidth={800} p={6} m="10px auto" as="form" onSubmit={handleSubmit(createUser)}>
+
+            <Form1 />
+
+            <Form2 />
+
+          <ButtonGroup mt="5%" w="100%">
+            <Flex w="100%" justifyContent="space-between">
+                <Button w="7rem" colorScheme="blue" variant="solid" type='submit'>
+                  Submit
+                </Button>
+            </Flex>
+          </ButtonGroup>
+        </Box>
+      </>
+    );
+  }
+
   return (
-    <>
-      <Box borderWidth="1px" rounded="lg" shadow="1px 1px 3px rgba(0,0,0,0.3)" maxWidth={800}
-        p={6} m="10px auto" as="form">
-
-          <Form1 />
-
-          <Form2 />
-
-          <Form3 />
-
-
-        <ButtonGroup mt="5%" w="100%">
-          <Flex w="100%" justifyContent="space-between">
-              <Button w="7rem" colorScheme="blue" variant="solid"
-                onClick={() => {
-                  toast({
-                    title: 'Account created.',
-                    description: "We've created your account for you.",
-                    status: 'success',
-                    duration: 3000,
-                    isClosable: true,
-                  });
-                }}>
-                Submit
-              </Button>
-          </Flex>
-        </ButtonGroup>
-      </Box>
-    </>
-  );
+    <div> <Multistep /> </div>
+  )
 }
