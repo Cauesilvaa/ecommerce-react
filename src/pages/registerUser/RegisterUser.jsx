64b6c0fbc2a868a60c2956cd534bb3cee2e6cdf2
Cauesilvaa <43147371+Cauesilvaa@users.noreply.github.com>
2023-06-30 11:05:21 -0300

    feat: form register user finished

--- src/pages/registerUser/RegisterUser.jsx
+++ src/pages/registerUser/RegisterUser.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import {
-  Progress,
   Box,
   ButtonGroup,
   Button,
@@ -11,26 +10,22 @@ import {
   FormLabel,
   Input,
   Select,
-  SimpleGrid,
-  InputLeftAddon,
   InputGroup,
-  Textarea,
-  FormHelperText,
   InputRightElement,
 } from '@chakra-ui/react';
 
-import { useToast } from '@chakra-ui/react';
 import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { object, string } from 'yup';
+import SpanError from '../../components/spanError/SpanError';
 
-const schema = object({
-  name: string().required()
-})
+// const schema = object({
+//   name: string().required()
+// })
 
 export default function RegisterUser (){
 
-  const { register, handleSubmit, formState: { errors } } = useForm({resolver: yupResolver(schema)})
+  const { register, handleSubmit, formState: { errors } } = useForm(/*{resolver: yupResolver(schema)}*/)
 
   function createUser(data) {
     console.log(data);
@@ -41,24 +36,20 @@ export default function RegisterUser (){
     const handleClick = () => setShow(!show);
     return (
       <>
-        <Heading w="100%" textAlign={'center'} fontWeight="normal" mb="2%">
-          Realize seu cadastro
-        </Heading>
+        <Heading w="100%" textAlign={'center'} fontWeight="normal" mb="2%"> Realize seu cadastro </Heading>
 
         <Flex>
 
           <FormControl mr="5%">
-            <FormLabel htmlFor="first-name" fontWeight={'normal'}>
-              Nome
-            </FormLabel>
-            <Input id="first-name" placeholder="Nome" {...register('name')}/>
+            <FormLabel htmlFor="first-name" fontWeight={'normal'}> Nome </FormLabel>
+            <Input id="first-name" placeholder="Nome" {...register('first_name', { required: true })}/>
+            {errors.first_name && <SpanError message={'Campo obrigatorio'} />}
           </FormControl>
 
           <FormControl>
-            <FormLabel htmlFor="last-name" fontWeight={'normal'}>
-              Sobrenome
-            </FormLabel>
-            <Input id="last-name" placeholder="Sobrenome" {...register('last_name')}/>
+            <FormLabel htmlFor="last-name" fontWeight={'normal'}> Sobrenome </FormLabel>
+            <Input id="last-name" placeholder="Sobrenome" {...register('last_name', { required: true })}/>
+            {errors.last_name && <SpanError message={'Campo obrigatorio'} />}
           </FormControl>
 
         </Flex>
@@ -66,17 +57,15 @@ export default function RegisterUser (){
         <Flex marginTop={'30'}>
 
           <FormControl mr="5%">
-            <FormLabel htmlFor="birth" fontWeight={'normal'}>
-              Data de Nascimento
-            </FormLabel>
-            <Input id="birth" type='date' {...register('birth')} />
+            <FormLabel htmlFor="birth" fontWeight={'normal'}> Data de Nascimento </FormLabel>
+            <Input id="birth" type='date' {...register('birth', { required: true })} />
+            {errors.birth && <SpanError message={'Campo obrigatorio'} />}
           </FormControl>
 
           <FormControl>
-            <FormLabel htmlFor="cpf" fontWeight={'normal'}>
-              CPF
-            </FormLabel>
-            <Input id="cpf" placeholder="000.000.000-00" {...register('cpf')}/>
+            <FormLabel htmlFor="cpf" fontWeight={'normal'}> CPF </FormLabel>
+            <Input id="cpf" placeholder="000.000.000-00" {...register('cpf', { required: true })}/>
+            {errors.cpf && <SpanError message={'Campo obrigatorio'} />}
           </FormControl>
 
         </Flex>
@@ -84,25 +73,23 @@ export default function RegisterUser (){
         <Flex marginTop={'30'}>
 
           <FormControl mr="5%">
-            <FormLabel htmlFor="email" fontWeight={'normal'}>
-              Email
-            </FormLabel>
-            <Input id="email" placeholder="email@email.com" {...register('email')}/>
+            <FormLabel htmlFor="email" fontWeight={'normal'}> Email </FormLabel>
+            <Input id="email" placeholder="email@email.com" {...register('email', { required: true })}/>
+            {errors.email && <SpanError message={'Campo obrigatorio'} />}
           </FormControl>
 
           <FormControl>
-          <FormLabel htmlFor="password" fontWeight={'normal'} mt="2%">
-            Senha
-          </FormLabel>
+          <FormLabel htmlFor="password" fontWeight={'normal'} mt="2%"> Senha </FormLabel>
           
           <InputGroup size="md">
-            <Input pr="4.5rem" type={show ? 'text' : 'password'} placeholder="Senha" {...register('password')}/>
+            <Input pr="4.5rem" type={show ? 'text' : 'password'} placeholder="Senha" {...register('password', { required: true })}/>            
             <InputRightElement width="4.5rem">
               <Button h="1.75rem" size="sm" onClick={handleClick}>
                 {show ? 'Esconder' : 'Exibir'}
               </Button>
             </InputRightElement>
           </InputGroup>
+          {errors.password && <SpanError message={'Campo obrigatorio'} />}
         </FormControl>
 
         </Flex>
@@ -128,11 +115,12 @@ export default function RegisterUser (){
             shadow="sm"
             size="sm"
             w="full"
-            rounded="md" {...register('country')}>
+            rounded="md" {...register('country', { required: true })}>
             <option>United States</option>
             <option>Canada</option>
             <option>Mexico</option>
           </Select>
+          {errors.country && <SpanError message={'Campo obrigatorio'} />}
         </FormControl>
 
         <FormControl as={GridItem} colSpan={[6, 6, null, 2]}>
@@ -141,9 +129,7 @@ export default function RegisterUser (){
             fontSize="sm"
             fontWeight="md"
             color="gray.700"
-            _dark={{
-              color: 'gray.50',
-            }}
+            _dark={{ color: 'gray.50' }}
             mt="2%">
             CEP
           </FormLabel>
@@ -157,24 +143,23 @@ export default function RegisterUser (){
             size="sm"
             w="full"
             rounded="md"
-            {...register('cep')}
+            {...register('cep', { required: true })}
           />
+          {errors.cep && <SpanError message={'Campo obrigatorio'} />}
         </FormControl>
 
         <Flex>
 
           <FormControl mr="5%" marginTop={'20px'}>
-            <FormLabel htmlFor="street" fontWeight={'normal'}>
-              Endereço
-            </FormLabel>
-            <Input id="street" type='text' {...register('street')}/>
+            <FormLabel htmlFor="street" fontWeight={'normal'}> Endereço </FormLabel>
+            <Input id="street" type='text' {...register('street', { required: true })}/>
+            {errors.street && <SpanError message={'Campo obrigatorio'} />}
           </FormControl>
 
           <FormControl marginTop={'20px'}>
-            <FormLabel htmlFor="complement" fontWeight={'normal'}>
-              Complemento
-            </FormLabel>
-            <Input id="complement" type='text' {...register('complement')}/>
+            <FormLabel htmlFor="complement" fontWeight={'normal'}> Complemento </FormLabel>
+            <Input id="complement" type='text' {...register('complement', { required: true })}/>
+            {errors.complement && <SpanError message={'Campo obrigatorio'} />}
           </FormControl>
 
         </Flex>
@@ -182,17 +167,15 @@ export default function RegisterUser (){
         <Flex>
 
           <FormControl mr="5%" marginTop={'20px'}>
-            <FormLabel htmlFor="city" fontWeight={'normal'}>
-              Cidade
-            </FormLabel>
-            <Input id="city" type='text' {...register('city')}/>
+            <FormLabel htmlFor="city" fontWeight={'normal'}> Cidade </FormLabel>
+            <Input id="city" type='text' {...register('city', { required: true })}/>
+            {errors.city && <SpanError message={'Campo obrigatorio'} />}
           </FormControl>
 
           <FormControl marginTop={'20px'}>
-            <FormLabel htmlFor="state" fontWeight={'normal'}>
-              Estado
-            </FormLabel>
-            <Input id="state" type='text' {...register('state')}/>
+            <FormLabel htmlFor="state" fontWeight={'normal'}> Estado </FormLabel>
+            <Input id="state" type='text' {...register('state', { required: true })}/>
+            {errors.state && <SpanError message={'Campo obrigatorio'} />}
           </FormControl>
 
         </Flex>
@@ -201,9 +184,6 @@ export default function RegisterUser (){
   };
 
   function Multistep() {
-    const toast = useToast();
-    const [step, setStep] = useState(1);
-    const [progress, setProgress] = useState(33.33);
     return (
       <>
         <Box borderWidth="1px" rounded="lg" shadow="1px 1px 3px rgba(0,0,0,0.3)" maxWidth={800} p={6} m="10px auto" as="form" onSubmit={handleSubmit(createUser)}>
@@ -214,9 +194,7 @@ export default function RegisterUser (){
 
           <ButtonGroup mt="5%" w="100%">
             <Flex w="100%" justifyContent="space-between">
-                <Button w="7rem" colorScheme="blue" variant="solid" type='submit'>
-                  Submit
-                </Button>
+                <Button w="7rem" colorScheme="blue" variant="solid" type='submit'> Submit </Button>
             </Flex>
           </ButtonGroup>
         </Box>
@@ -224,7 +202,5 @@ export default function RegisterUser (){
     );
   }
 
-  return (
-    <div> <Multistep /> </div>
-  )
+  return ( <> <Multistep /> </> )
 }
