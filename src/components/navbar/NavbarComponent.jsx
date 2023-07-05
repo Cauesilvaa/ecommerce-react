8f8181b1d5a8775550b19964e073824908d0cdc0
Cauesilvaa <43147371+Cauesilvaa@users.noreply.github.com>
2023-07-05 12:16:41 -0300

    Navbar com mudança de tema

--- src/components/navbar/NavbarComponent.jsx
+++ src/components/navbar/NavbarComponent.jsx
@@ -1,56 +1,3 @@
-/*
-import './Navbar.css'
-import React from 'react'
-import imgLogo from '../../assets/img-login.jpg'
-import { BsFillCartFill } from "react-icons/bs";
-
-import { useNavigate } from 'react-router';
-
-const NavbarComponent = () => {
-
-  const navigate = useNavigate()
-
-  return (
-    <div>
-
-<nav className="navbar navbar-expand-lg bg-body-tertiary class-navbar" bg="light" expand="lg">
-  <div className="container-fluid">
-    <img src={imgLogo} alt="" className='class-img'/>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0 class-color-text">
-        <li className="nav-item">
-          <a className="nav-link" aria-current="page" href="" onClick={()=> navigate('/home')}>Início</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Contato</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Sobre</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Minha conta</a>
-        </li>
-        <li className="nav-item">
-        <a className="nav-link" href="#"> <BsFillCartFill /> </a>
-        </li>
-      </ul>
-      <form className="d-flex search-navbar" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-light" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-    </div>
-  )
-}
-
-export default NavbarComponent
-*/
-
 import {
   Box,
   Flex,
@@ -67,10 +14,13 @@ import {
   useDisclosure,
   useColorModeValue,
   Stack,
+  useColorMode,
+  Center
 } from '@chakra-ui/react';
-import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from '@chakra-ui/icons';
 import imgLogo from '../../assets/img-login.jpg';
 import './Navbar.css';
+import { useNavigate } from 'react-router-dom';
 
 const Links = ['Minha conta', 'Contato', 'Sobre'];
 
@@ -90,46 +40,83 @@ const NavLink = ({ children }) => (
 
 export default function NavbarComponent() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const { colorMode, toggleColorMode } = useColorMode();
+  const navigate = useNavigate()
 
   return (
     <>
       <Box bg={useColorModeValue('#539CCF', 'gray.900')} px={4}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <IconButton
-            size={'md'}
-            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            aria-label={'Open Menu'}
-            display={{ md: 'none' }}
-            onClick={isOpen ? onClose : onOpen}
-          />
-          <HStack spacing={8} alignItems={'center'}>
-            {/* <Box>{imgLogo}</Box> */}
-            <img src={imgLogo} alt="" className='class-img'/>
-            <HStack
-              color={'white'}
-              as={'nav'}
-              spacing={4}
-              display={{ base: 'none', md: 'flex' }}>
-              {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
-              ))}
-            </HStack>
-          </HStack>
-          <form className="d-flex search-navbar" role="search">
-            <input className="form-control me-2" type="search" placeholder="..." aria-label="Search" />
-            <button className="btn btn-outline-light btn-search" type="submit">Buscar</button>
-          </form>
-        </Flex>
+        <Stack direction={'row'} spacing={7}>        
 
-        {isOpen ? (
-          <Box pb={4} display={{ md: 'none' }}>
-            <Stack as={'nav'} spacing={4}>
-              {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
-              ))}
-            </Stack>
-          </Box>
-        ) : null}
+          <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+            
+            <IconButton
+              size={'md'}
+              icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+              aria-label={'Open Menu'}
+              display={{ md: 'none' }}
+              onClick={isOpen ? onClose : onOpen}
+            />
+
+            <HStack spacing={8} alignItems={'center'}>
+              {/* <Box>{imgLogo}</Box> */}
+              <img src={imgLogo} alt="" className='class-img'/>
+              <HStack color={'white'} as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
+                {Links.map((link) => ( <NavLink key={link}>{link}</NavLink>))}
+              </HStack>
+            </HStack>
+
+            <form className="d-flex search-navbar" role="search">
+              <input className="form-control me-2" type="search" placeholder="..." aria-label="Search" />
+              <button className="btn btn-outline-light btn-search" type="submit">Buscar</button>
+            </form>
+
+            <Button onClick={toggleColorMode} marginRight={'2%'}>
+                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+              </Button>
+
+              <Menu>
+                <MenuButton 
+                  marginRight={'2%'}
+                  as={Button}
+                  rounded={'full'}
+                  variant={'link'}
+                  cursor={'pointer'}
+                  minW={0}>
+                  <Avatar size={'sm'} src={'https://avatars.dicebear.com/api/male/username.svg'} />
+                </MenuButton>
+
+                <MenuList alignItems={'center'}>
+
+                  <br />
+                  <Center>
+                    <Avatar size={'2xl'} src={'https://avatars.dicebear.com/api/male/username.svg'} />
+                  </Center>
+                  
+                  <br />
+                  <Center>
+                    <p>Username</p>
+                  </Center>
+
+                  <br />
+                  <MenuDivider />
+
+                  <MenuItem>Your Servers</MenuItem>
+                  <MenuItem>Account Settings</MenuItem>
+                  <MenuItem onClick={() => { navigate('/login') }}>Logout</MenuItem>
+                </MenuList>
+              </Menu>
+
+          </Flex>
+
+          {isOpen ? (
+            <Box pb={4} display={{ md: 'none' }}>
+              <Stack as={'nav'} spacing={4}>
+                {Links.map((link) => ( <NavLink key={link}>{link}</NavLink> ))}
+              </Stack>
+            </Box>
+          ) : null}
+        </Stack>
       </Box>
     </>
   );
